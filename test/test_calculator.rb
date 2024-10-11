aaf230a5db203a7648870256686771fdfa5b7972
Kyle Verwey <108106818+starzzots@users.noreply.github.com>
2024-10-11 23:37:42 +0000

    added notes to the ruby test_calc file for fast reference

--- test/test_calculator.rb
+++ test/test_calculator.rb
@@ -1,9 +1,9 @@
-require 'minitest/autorun'
-require './calculator'
+require 'minitest/autorun' #require 'minitest/autorun': This line loads the MiniTest framework, which we bundle installed to make it available in our codespace
+require './calculator' #require './calculator': This line loads our Calculator class.
 
-class TestCalculator < Minitest::Test
-  def test_addition
+class TestCalculator < Minitest::Test #class TestCalculator < Minitest::Test: We define a test class that inherits from Minitest::Test.
+  def test_addition #def test_addition: Our first test method. In MiniTest, any method that begins with test_ is automatically run as a test.
     calculator = Calculator.new
-    assert_equal 4, calculator.add(2, 2), "Addition method failed"
+    assert_equal 4, calculator.add(2, 2), "Addition method failed" #assert_equal 4, calculator.add(2, 2): The assertion. We’re checking if the add method returns 4 when given 2 and 2.
   end
 end
